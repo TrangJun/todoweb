48d61240518dcab6ef4ce728919593fa04016eed
TrangJun <thutranght97@gmail.com>
2022-05-26 18:15:50 +0700

    sua router post /

--- app.js
+++ app.js
@@ -2,35 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config');
-const username = "Trang"
-const password = "thutranght29071997";
-const cluster = "cluster0.nsqam";
-const dbname = "todo";
-//Middlewares
 
 //Routes
 const todoRoute = require('./routes/todo.js');
 app.use('/todo', todoRoute );
-// app.post('/post',(req, res) =>{
-//     res.send('home page');
-// })
-//connect db
-const db = process.env.DB_CONNECTION;
-console.log(db);
-// mongoose.connect("mongodb+srv://Trang:thutranght29071997@cluster0.nsqam.mongodb.net/todo?retryWrites=true&w=majority",
-// {
-//     useNewUrlParser: true,
-// }, ()=> {
-//     console.log("connect to mongodb");
-// });
-
+console.log(process.env.DB_CONNECTION)
 mongoose.connect(process.env.DB_CONNECTION,
 {
     useNewUrlParser: true,
 }, ()=> {
     console.log("connect to mongodb");
 });
-
-//How to we start listening to the server
 
 app.listen(3000);
--- routes/todo.js
+++ routes/todo.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-const tasksModel = require('../models/task.js');
+
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const todoController = require('../controllers/todoController.js')
-// router.get('/', (req, res)=>{
-//     res.send('router todo')
-// });
 
-router.post('/addTask', urlencodedParser,todoController.addTask);
+router.post('/', urlencodedParser,todoController.addTask);
 
 router.get('/', urlencodedParser, todoController.getTaskList);
 
